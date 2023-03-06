abe7781f7b3de9f8d6b3c6b9a69969402c2d58cb
Guy Marshall <guymarshall9@gmail.com>
2023-03-06 22:14:00 +0000

    Add array_to_string_with_quotes

--- functions.php
+++ functions.php
@@ -22,6 +22,14 @@ function add_quotes(string $input): string
     return '"'.$input.'"';
 }
 
+function array_to_string_with_quotes(array $input): string {
+    $output = array_map(function ($value) {
+        return add_quotes(strval($value));
+    }, $input);
+
+    return implode(", ", $output);
+}
+
 function generate_initials(string $first_name = '', string $middle_name = '', string $last_name = ''): string
 {
     return $first_name[0].$middle_name[0].$last_name[0];
