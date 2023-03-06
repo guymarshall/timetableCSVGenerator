79e4f75eeb20665a00f35ab35368e2f5fca064cb
Guy Marshall <guymarshall9@gmail.com>
2023-03-06 22:12:16 +0000

    Add add_quotes

--- functions.php
+++ functions.php
@@ -17,6 +17,11 @@ function generate_csv(string $filename, array $field_headings, array $data): voi
     fclose($file);
 }
 
+function add_quotes(string $input): string
+{
+    return '"'.$input.'"';
+}
+
 function generate_initials(string $first_name = '', string $middle_name = '', string $last_name = ''): string
 {
     return $first_name[0].$middle_name[0].$last_name[0];
