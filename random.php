fafef012674f39eccbf53150829a9a6c8e0b5a03
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-03-06 12:32:26 +0000

    Add random_room

--- random.php
+++ random.php
@@ -13,3 +13,12 @@ function day_from_int(int $day_int): string
     $days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     return $days[$day_int];
 }
+
+function random_room(): string
+{
+    $rooms = ["Ma1", "Ma2", "Ma3", "Ma4", "Ma5", "Ma6", "Ma7", "Ma8", "Ma9", "DT1", "DT2", "DT3", "DT4", "DT5", "IT1", "IT2", "IT3", "La1", "La2", "La3", "La4", "La5", "History1", "History2", "History3", "Geography1", "Geography2", "Geography3", "Sc1", "Sc2", "Sc3", "Sc4", "Sc5", "Sc6", "Sc7", "Sc8", "Eng1", "Eng2", "Eng3", "Eng4", "Eng5", "Eng6", "Eng7", "Eng8", "Music1", "Music2", "Drama1", "Drama2", "PE"];
+
+    $min = 0;
+    $max = count($rooms) - 1;
+    return $rooms[rand($min, $max)];
+}
--- random.rs
+++ random.rs
@@ -4,14 +4,6 @@ use std::ops::{Range, RangeInclusive};
 use rand::prelude::*;
 use rand::distributions::uniform::Uniform;
 
-pub fn random_room() -> &'static str {
-    let rooms: [&str; 49] = ["Ma1", "Ma2", "Ma3", "Ma4", "Ma5", "Ma6", "Ma7", "Ma8", "Ma9", "DT1", "DT2", "DT3", "DT4", "DT5", "IT1", "IT2", "IT3", "La1", "La2", "La3", "La4", "La5", "History1", "History2", "History3", "Geography1", "Geography2", "Geography3", "Sc1", "Sc2", "Sc3", "Sc4", "Sc5", "Sc6", "Sc7", "Sc8", "Eng1", "Eng2", "Eng3", "Eng4", "Eng5", "Eng6", "Eng7", "Eng8", "Music1", "Music2", "Drama1", "Drama2", "PE"];
-
-    let mut rng: ThreadRng = thread_rng();
-    let range: RangeInclusive<usize> = 0..=rooms.len() - 1;
-    &rooms[rng.gen_range(range)]
-}
-
 pub fn random_length_random_vector() -> Vec<i32> {
     let mut rng: ThreadRng = thread_rng();
     let length_range: RangeInclusive<usize> = 1..=11;
