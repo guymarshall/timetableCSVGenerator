2aa8cc14e963c0586f2c4e5b39da59745889fbba
Guy Marshall <guymarshall9@gmail.com>
2023-03-07 21:57:31 +0000

    Remove generate_csv from functions.php

--- functions.php
+++ functions.php
@@ -1,22 +1,5 @@
 <?php
 
-function generate_csv(string $filename, array $field_headings, array $data): void {
-    $file = fopen($filename, "w");
-    if ($file === false) {
-        die("Couldn't create $filename");
-    }
-
-    $headings = explode(",", rtrim(implode(",", $field_headings), ","));
-    fputcsv($file, $headings);
-
-    foreach ($data as $record) {
-        $row = explode(",", rtrim(implode(",", $record), ","));
-        fputcsv($file, $row);
-    }
-
-    fclose($file);
-}
-
 function add_quotes(string $input): string {
     return '"'.$input.'"';
 }
