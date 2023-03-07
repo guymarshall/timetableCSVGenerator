eb631be214be68cdd036cff07d2626137114cc85
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-03-07 12:22:40 +0000

    Remove generate_curriculum_csv - data changing will happen outside the function

--- functions.php
+++ functions.php
@@ -36,25 +36,3 @@ function generate_initials(string $first_name = '', string $middle_name = '', st
 {
     return $first_name[0].$middle_name[0].$last_name[0];
 }
-
-function generate_curriculum_csv($filename, $field_headings, $data) {
-    $file = fopen($filename, "w");
-    if ($file === false) {
-        die("Couldn't create $filename");
-    }
-
-    $headings = implode(",", $field_headings);
-    if (fwrite($file, $headings . "\n") === false) {
-        die("Couldn't write to $filename");
-    }
-
-    foreach ($data as $record) {
-        $line_including_trailing_comma = implode(",", $record);
-        $line = rtrim($line_including_trailing_comma, ",");
-        if (fwrite($file, $line . "\n") === false) {
-            die("Couldn't write to $filename");
-        }
-    }
-
-    fclose($file);
-}
