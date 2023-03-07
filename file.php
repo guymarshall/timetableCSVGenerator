e4305ae9ec7cf76983e847f4cac408d0312f52af
Guy Marshall <guymarshall9@gmail.com>
2023-03-07 20:38:30 +0000

    Add get_names

--- file.php
+++ file.php
@@ -14,3 +14,14 @@ function file_to_array(string $filename): array|false {
 
     return $words;
 }
+
+function get_names(string $filename): array
+{
+    $names = file_to_array($filename);
+    if ($names === false) {
+        echo "An error occurred when trying to open the file.";
+        exit(1);
+    }
+
+    return $names;
+}
