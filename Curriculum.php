1fa9d239aa1f69940dc727f89db8e60aba7237d8
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-03-08 12:27:24 +0000

    Remove rust code in curriculum class

--- Curriculum.php
+++ Curriculum.php
@@ -17,26 +17,4 @@ class Curriculum {
 
         fclose($file);
     }
-
-    /*pub fn generate_curriculum_csv(filename: &str, field_headings: Vec<&str>, data: Vec<Vec<i32>>) {
-        let path: &Path = Path::new(filename);
-        let mut file: File = match File::create(&path) {
-            Err(why) => panic!("couldn't create {}: {}", path.display(), why),
-            Ok(file) => file,
-        };
-
-        let headings_including_trailing_comma: String = field_headings.iter().map(|&heading| heading.to_string() + ",").collect();
-        let headings: String = headings_including_trailing_comma[0..].to_string();
-        if let Err(why) = writeln!(file, "{}", headings) {
-            panic!("couldn't write to {}: {}", path.display(), why);
-        }
-
-        for record in data {
-            let line_including_trailing_comma: String = record.iter().map(|&cell| cell.to_string() + ",").collect();
-            let line: String = line_including_trailing_comma[0..].to_string();
-            if let Err(why) = writeln!(file, "{}", line) {
-                panic!("couldn't write to {}: {}", path.display(), why);
-            }
-        }
-    }*/
 }
