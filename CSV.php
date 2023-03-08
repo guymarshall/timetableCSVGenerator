5c11cfa54f648598d5a414fa6a9353d8de4c17d7
Guy Marshall <guymarshall9@gmail.com>
2023-03-08 21:27:21 +0000

    Create CSV.php with CSV class to write data to CSV files

---
+++ CSV.php
@@ -0,0 +1,24 @@
+<?php
+
+class CSV
+{
+    public function generate_csv(string $filename, array $field_headings, array $data): void
+    {
+        $file = fopen($filename, "w");
+        if ($file === false)
+        {
+            die("Couldn't create $filename");
+        }
+
+        $headings = explode(",", rtrim(implode(",", $field_headings), ","));
+        fputcsv($file, $headings);
+
+        foreach ($data as $record)
+        {
+            $row = explode(",", rtrim(implode(",", $record), ","));
+            fputcsv($file, $row);
+        }
+
+        fclose($file);
+    }
+}
